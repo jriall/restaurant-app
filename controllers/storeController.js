a350113d5ec925985aa9b6ed1b3de0b640bb938e
JamesRiall <jamesrariall@gmail.com>
2018-01-06 12:44:19 +0000

    complete to the end of lesson 13

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -7,7 +9,16 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  await store.save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect('/store/${store.slug}');
+};
+
+exports.getStores = async (req, res) => {
+  //1.Query the database for a list of all stores
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores });
 }

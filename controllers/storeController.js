de95e6db3c3623cd9cb2a691b9d2ef6d74734578
JamesRiall <jamesrariall@gmail.com>
2018-01-07 12:12:59 +0000

    completed up to end of lesson 21

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
-const multer = require('multer');
-const jimp = require('jimp');
-const uuid = require('uuid');
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
 
 const multerOptions = {
   storage: multer.memoryStorage(),
   fileFilter(req, file, next) {
-    const isPhoto = file.mimetype.startsWith('image/');
-    if(isPhoto) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
       next(null, true);
     } else {
-      next({ message: 'That file type isn\'t allowed!' }, false);
+      next({ message: "That file type isn't allowed!" }, false);
     }
   }
-}
+};
 
 exports.homePage = (req, res) => {
-  res.render('index');
+  res.render("index");
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store' });
+  res.render("editStore", { title: "Add Store" });
 };
 
-exports.upload = multer(multerOptions).single('photo');
+exports.upload = multer(multerOptions).single("photo");
 
 exports.resize = async (req, res, next) => {
   if (!req.file) {
     next();
     return;
   }
-  const extension = req.file.mimetype.split('/')[1];
+  const extension = req.file.mimetype.split("/")[1];
   req.body.photo = `${uuid.v4()}.${extension}`;
   //resize
   const photo = await jimp.read(req.file.buffer);
@@ -42,17 +42,20 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = await (new Store(req.body)).save();
+  const store = await new Store(req.body).save();
   await store.save();
-  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-  res.redirect('/store/${store.slug}');
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect("/store/${store.slug}");
 };
 
 exports.getStores = async (req, res) => {
   //1.Query the database for a list of all stores
   const stores = await Store.find();
   console.log(stores);
-  res.render('stores', { title: 'Stores', stores });
+  res.render("stores", { title: "Stores", stores });
 };
 
 exports.editStore = async (req, res) => {
@@ -61,25 +64,33 @@ exports.editStore = async (req, res) => {
   //2. Confirm they are the owner of the store
   //TODO
   //3. Render out the edit form so the user can update their store
-  res.render('editStore', { title: `Edit ${store.name}`, store });
+  res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
   //set the location data to be a point
-  req.body.location.type = 'Point';
+  req.body.location.type = "Point";
   //find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
     runValidators: true
   }).exec();
-  req.flash('success', `Successfully updated <strong>${store.name}<strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}<strong>. <a href="/stores/${store.slug}">View Store</a>`
+  );
   res.redirect(`/stores/${store._id}/edit`);
   //redirect them the store and tell them it worked
-}
+};
 
 exports.getStoreBySlug = async (req, res, next) => {
   const store = await Store.findOne({ slug: req.params.slug });
   if (!store) return next();
-  res.render('store', { store, title: store.name });
+  res.render("store", { store, title: store.name });
+};
 
-}
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags' });
+};
